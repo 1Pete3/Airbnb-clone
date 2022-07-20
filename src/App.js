e66e18e94d36c0627b803c86a4181d29a05d89b3
1pete3 <skowronekpeter103@gmail.com>
2022-07-20 13:09:50 -0400

    Added view ports and edited how data was handled

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 import data from '../src/data';
-const cardData = data.map((x) => (
-  <Card
-    title={x.title}
-    price={x.price}
-    rating={x.stats.rating}
-    reviewCount={x.stats.reviewCount}
-    coverImg={x.coverImg}
-    location={x.location}
-  />
-));
+const cardData = data.map((x) => <Card key={x.key} x={x} />);
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <section className="cardList">{cardData}</section>
+      <section className="cards-list">{cardData}</section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,27 @@
 import star from './images/Star 1.png';
+
 function Card(props) {
+  let badgeText;
+  if (props.x.openSpots === 0) {
+    badgeText = 'SOLD OUT';
+  } else if (props.x.location === 'Online') {
+    badgeText = 'Online';
+  } else {
+    badgeText = '';
+  }
   return (
-    <div className="Card">
-      <img src={props.coverImg} alt="pic" className="card--image"></img>
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={props.x.coverImg} alt="pic" className="card--image"></img>
       <div className="card--stats">
         <img src={star} alt="Star pic" className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.location}</span>
+        <span>{props.x.stats.rating}</span>
+        <span className="gray">({props.x.stats.reviewCount}) • </span>
+        <span className="gray">{props.x.location}</span>
       </div>
-      <p className="card--title">{props.title}</p>
+      <p className="card--title">{props.x.title}</p>
       <p className="card--price">
-        <span className="bold">From ${props.price}</span> / person
+        <span className="bold">From ${props.x.price}</span> / person
       </p>
     </div>
   );
